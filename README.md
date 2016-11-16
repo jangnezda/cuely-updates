03e94d0ff84a0627190534875c31f35fbc5da0b7
Jan Gnezda <jan@pipetop.com>
2016-11-16 11:58:52 +0100

    Few changes to support local filesystem for development env

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cuely-updates
 Minimal server for Cuely app updates. Inspired by blog post on `http://electron.rocks/publishing-for-os-x/` and related repo `https://github.com/Vj3k0/ea-todo-server`.
+
+## Development / local
+Create a dir `updates/latest/osx` in the project and copy builds inside that dir. Then run with `npm run dev`. To get the update json: `curl -v http://localhost:5123/updates/latest/v=x.y.z
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-use strict';
+'use strict';
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -12,46 +13,57 @@ const FETCH_INTERVAL = 300000;
 app.use(require('morgan')('dev'));
 
 if (isDevelopment) {
-    app.use('/updates/latest', express.static(path.join(__dirname, 'updates/latest')));
+  app.use('/updates/latest', express.static(path.join(__dirname, 'updates/latest')));
 }
 
 app.get('/updates/latest', (req, res) => {
-    if (version) {
-        const clientVersion = req.query.v;
+  if (version) {
+    const clientVersion = req.query.v;
 
-        if (clientVersion === version) {
-            res.status(204).end();
-        } else {
-            res.json({
-                url: `${getBaseUrl()}/updates/latest/osx/eatodo-${version}-mac.zip`
-            });
-        }
+    if (clientVersion === version) {
+      res.status(204).end();
+    } else {
+      res.json({
+        url: `${getBaseUrl()}/updates/latest/osx/cuely-${version}-osx.zip`
+      });
     }
-    else {
-        res.status(204).end();
-    }
+  } else {
+    res.status(204).end();
+  }
 });
 
 let getBaseUrl = () => {
-    if (isDevelopment) {
-        return 'http://localhost:5123';
-    } else {
-        return 'http://eatodo.s3.amazonaws.com'
-    }
+  if (isDevelopment) {
+    return 'http://localhost:5123';
+  } else {
+    return 'http://eatodo.s3.amazonaws.com'
+  }
+}
+
+let getLatestRelease = () => {
+  const dir = `${__dirname}/updates/latest/osx`;
+
+  const versionsDesc = fs.readdirSync(dir).filter(file => file.endsWith('.zip')).map(file => file.split('-')[1]).reverse();
+  return versionsDesc[0];
 }
 
 let getVersion = () => {
+  if (isDevelopment) {
+    version = getLatestRelease();
+  } else {
     console.log(`Fetching latest version from ${versionUrl}`);
     request.get(versionUrl, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            version = body;
-        }
-        else if (error) {
-            console.error(error);
-        }
+      if (!error && response.statusCode == 200) {
+        version = body;
+      }
+      else if (error) {
+        console.error(error);
+      }
     });
+  }
 
-    setTimeout(getVersion, FETCH_INTERVAL);
+  console.log("Latest version is:", version);
+  setTimeout(getVersion, FETCH_INTERVAL);
 }
 
 const versionUrl = `${getBaseUrl()}/updates/latest/osx/VERSION`;
